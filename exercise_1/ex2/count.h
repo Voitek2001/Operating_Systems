9ac0783c2ad948105a7f5cf80913a709d237171d
wojtek <wsuski@student.agh.edu.pl>
2023-03-08 18:25:59 +0100

    Zadanie2

---
+++ exercise_1/ex2/count.h
@@ -0,0 +1,21 @@
+#ifndef BLOCK_MANAGEMENT_H
+#define BLOCK_MANAGEMENT_H
+#include <stdbool.h>
+
+
+typedef struct ArrayOfBlocks {
+	int capacity;
+	int amount;
+	char** ptr;
+	bool* deleted;
+} ArrayOfBlocks;
+
+
+ArrayOfBlocks* create_empty_struct(int size); 
+int count_lines_and_words(ArrayOfBlocks * curr_struct, const char *file_name);
+char* show_at(ArrayOfBlocks * curr_struct, int index);
+void remove_at(ArrayOfBlocks * curr_struct ,int index);
+void destroy(ArrayOfBlocks * curr_struct);
+
+#endif
+
