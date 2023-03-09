00263500de67ae4e616bbbf303d478d24811f0d0
wojtek <wsuski@student.agh.edu.pl>
2023-03-09 20:52:54 +0100

    add ready exercise

---
+++ SuskiWojciech/cw01/zad3a/count_dll.h
@@ -0,0 +1,21 @@
+#ifndef BLOCK_MANAGEMENT_H
+#define BLOCK_MANAGEMENT_H
+#include <stdbool.h>
+
+
+typedef struct ArrayOfBlocks {
+	int capacity;
+	int amount;
+	char** ptr;
+	bool* deleted;
+} ArrayOfBlocks;
+
+
+ArrayOfBlocks* (*create_empty_struct)(int); 
+void (*count_lines_and_words)(ArrayOfBlocks * , const char *);
+char* (*show_at)(ArrayOfBlocks * , int);
+void (*remove_at)(ArrayOfBlocks *  ,int);
+void (*destroy)(ArrayOfBlocks *);
+
+#endif
+
