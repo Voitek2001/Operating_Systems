768850a62e03dfd6162f58bdfdd16f0dd4307b2d
wojtek <wsuski@student.agh.edu.pl>
2023-03-14 01:08:12 +0100

    clean up code

--- SuskiWojciech/cw02/zad4/count_size.c
+++ SuskiWojciech/cw02/zad4/count_size.c
@@ -31,43 +31,19 @@ int fn(const char *fpath, const struct stat *sb,
 
 
 int main(int argc, char** argv) {
-	struct dirent *pDirent;
-	DIR *pDir;
 
 	if (argc != 2) {
 		fprintf(stderr, "Invalid number of args");
 		return 1;
 	}
 	
-	pDir = opendir(argv[1]);
-	ftw(argv[1], fn, 10);
-	
+	if (ftw(argv[1], fn, 10) == -1) {
+		fprintf(stderr, "ftw error");
+		return 1;
+	}
 
 
 	return 0;
-	/*
-	if (pDir == NULL) {
-		fprintf(stderr, "Cannot open directory");
-		return 1;
-	}
-	
-	struct stat statbuf;
-	int status;
-	while ((pDirent = readdir(pDir)) != NULL) {	
-
-		status = stat(pDirent->d_name, &statbuf);
-		if (status == -1) {
-			fprintf(stderr, "Failed getting info about file [%s]", pDirent->d_name);
-		}
-		if (S_ISDIR(statbuf.st_mode)) {
-			continue;
-		}
-		
-		printf("Nazwa: [%s] ", pDirent->d_name);
-		printf("Rozmiar: %ld \n", statbuf.st_size);
-	}
-	closedir(pDir);
-	return 0;*/
 }
 
 
